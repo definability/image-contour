fdf737251cddfd817f05ec6f7d9cbe0a16faf1e3
char-lie <5962274+char-lie@users.noreply.github.com>
2015-08-23 20:16:15 +0300

    Added 'drawLine' function with circles

--- contour.js
+++ contour.js
@@ -1,23 +1,35 @@
 (function () {
+    var line = []
+    var ctx;
+    function drawLine (x, y) {
+        ctx.beginPath()
+        if (!line.length) {
+            ctx.moveTo(x, y)
+        } else {
+            ctx.moveTo(line[line.length-1].x, line[line.length-1].y)
+            ctx.lineTo(x, y)
+            ctx.stroke()
+        }
+        ctx.beginPath()
+            ctx.arc(x, y, 3, 0, 2*Math.PI)
+            ctx.stroke()
+            ctx.fillStyle='#CCCCCC'
+            ctx.fill()
+        line.push({x: x, y: y})
+    }
     window.onload = function () {
         var canvas = document.getElementById('canvas-main')
-        var ctx = document.getElementById('canvas-main').getContext('2d')
+        ctx = document.getElementById('canvas-main').getContext('2d')
+        ctx.strokeStyle='white'
+        ctx.lineWidth='1'
 
         var img = new Image()
         img.src = 'pluto.jpg'
         img.onload = function () {
             ctx.drawImage(img, 0, 0)
-            ctx.beginPath()
-            ctx.moveTo(0,0)
-            ctx.lineTo(700,700)
-            ctx.strokeStyle='white'
-            ctx.lineWidth='1'
-            ctx.stroke()
         }
         canvas.onclick = function (e) {
-            ctx.lineTo(e.layerX, e.layerY)
-            ctx.stroke()
-            console.log(e)
+            drawLine(e.layerX, e.layerY)
         }
     }
 })()
