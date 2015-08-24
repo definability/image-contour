30526252107921ae1c81995833a3afd30ec77747
char-lie <5962274+char-lie@users.noreply.github.com>
2015-08-24 19:37:00 +0300

    Added class for Polygon

---
+++ polygon.js
@@ -0,0 +1,23 @@
+var Polygon = function () {
+    if (arguments[0].length) {
+        this.points = arguments[0]
+    } else {
+        this.points = arguments
+    }
+    this.generateLines(this.points)
+}
+
+Polygon.prototype.getPoints = function () {
+    return this.points
+}
+
+Polygon.prototype.getLines = function () {
+    return this.lines
+}
+
+Polygon.prototype.generateLines = function (points) {
+    this.lines = []
+    for (var i=0; i<points.length; i++) {
+        this.lines.push({from: points[i], to: points[(i+1)%points.length]})
+    }
+}
