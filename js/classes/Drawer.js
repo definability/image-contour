33e2447e21eca08ce16a2eaec07463e898495706
char-lie <5962274+char-lie@users.noreply.github.com>
2015-08-24 20:27:38 +0300

    Moved 'beginPath' to 'render' function

--- js/classes/Drawer.js
+++ js/classes/Drawer.js
@@ -1,7 +1,6 @@
 var Drawer = function (canvasId) {
     this.canvas = document.getElementById(canvasId)
     this.context = this.canvas.getContext('2d')
-    this.context.beginPath()
     this.objects = []
     this.setup()
 }
@@ -17,7 +16,9 @@ Drawer.prototype.renderObject = function (obj) {
     throw new Error('Method is undefined')
 }
 
-Drawer.prototype.render = function () {
+Drawer.prototype.render = function (flag) {
+    this.context.clearRect(0, 0, this.canvas.width, this.canvas.height)
+    this.context.beginPath()
     this.objects.map(this.renderObject)
     this.context.stroke()
 }
