b0abee2971f04df33de9cb1f285b5b1bb50decfc
char-lie <5962274+char-lie@users.noreply.github.com>
2015-08-24 15:29:49 +0300

    Added 'setup' function for initial setup of drawer

--- drawer.js
+++ drawer.js
@@ -2,17 +2,20 @@ var Drawer = function (canvasId) {
     this.canvas = document.getElementById(canvasId)
     this.context = this.canvas.getContext('2d')
     this.objects = []
+    this.setup()
 }
 
-Drawer.prototype.add = function (objects) {
+Drawer.prototype.setup = function () {
+}
+
+Drawer.prototype.addObjects = function (objects) {
     this.objects = this.objects.concat(objects)
 }
 
-Drawer.prototype.renderObject = function () {
+Drawer.prototype.renderObject = function (obj) {
     throw new Error('Method is undefined')
 }
 
 Drawer.prototype.render = function () {
     this.objects.map(this.renderObject)
 }
-
