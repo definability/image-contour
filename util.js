467087116d2ee68df5a3b9d7def8463cf343ab45
char-lie <5962274+char-lie@users.noreply.github.com>
2015-08-24 15:28:24 +0300

    Added binding for inheritance

--- util.js
+++ util.js
@@ -1,10 +1,21 @@
-extend = function (child, parent) {
+function extend (parent, child) {
+    if (child === undefined) {
+        child = function () {
+            parent.prototype.constructor.apply(this, arguments)
+            for (var i in this) {
+                if (this[i].bind) {
+                    this[i] = this[i].bind(this)
+                }
+            }
+        }
+    }
+
     Object.keys(parent).filter(parent.hasOwnProperty).map(function (key) {
         child[key] = parent[key]
     })
 
     function ctor() {
-        this.constructor = child;
+        this.constructor = child
     }
     ctor.prototype = parent.prototype;
 
