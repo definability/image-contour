483c8669c83be8a54330cf00989408a20f3aea32
char-lie <5962274+char-lie@users.noreply.github.com>
2015-08-26 23:30:19 +0300

    Refactored: changed order of lines

--- js/classes/Drawer.Lines.js
+++ js/classes/Drawer.Lines.js
@@ -3,10 +3,10 @@ var LinesDrawer = extend(Drawer)
 LinesDrawer.prototype.renderObject = function (link) {
     var pointsIterator = link.getPointsIterator()
     var currentPoint = null
+    this.context.strokeStyle=link.highlight? '#AAAAFF':'#CCCCCC'
     this.context.beginPath()
     currentPoint = pointsIterator.next().value
     this.context.moveTo(currentPoint.x, currentPoint.y)
-    this.context.strokeStyle=link.highlight? '#AAAAFF':'#CCCCCC'
     currentPoint = pointsIterator.next().value
     this.context.lineTo(currentPoint.x, currentPoint.y)
     this.context.stroke()
