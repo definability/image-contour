9d4144f01d8c2b39ca82a36242da17d54808d40a
char-lie <5962274+char-lie@users.noreply.github.com>
2015-08-26 23:36:36 +0300

    Refactored: using ability of drawing by Points

--- js/classes/Drawer.Lines.js
+++ js/classes/Drawer.Lines.js
@@ -1,14 +1,11 @@
 var LinesDrawer = extend(Drawer)
 
 LinesDrawer.prototype.renderObject = function (link) {
-    var pointsIterator = link.getPointsIterator()
-    var currentPoint = null
     this.context.strokeStyle=link.highlight? '#AAAAFF':'#CCCCCC'
     this.context.beginPath()
-    currentPoint = pointsIterator.next().value
-    this.context.moveTo(currentPoint.x, currentPoint.y)
-    currentPoint = pointsIterator.next().value
-    this.context.lineTo(currentPoint.x, currentPoint.y)
+    var pointsIterator = link.getPointsIterator()
+    this.moveToPoint(pointsIterator.next().value)
+    this.lineToPoint(pointsIterator.next().value)
     this.context.stroke()
 }
 
